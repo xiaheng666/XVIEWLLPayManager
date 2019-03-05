157fe4a6736c868f903b67445e15d78f6c96c35e
xiaheng666 <key@xiaheng.net>
2019-03-05 19:31:45 +0800

    .a

--- Example/Pods/Headers/Private/XVIEWLLPayManager/LLPayUtil.h
+++
@@ -1,1 +0,0 @@
-../../../../../XVIEWLLPayManager/Classes/LLPayUtil.h
--- XVIEWLLPayManager/Classes/XVIEWLLPayManager.h
+++ XVIEWLLPayManager/Classes/XVIEWLLPayManager.h
@@ -17,16 +17,16 @@
 
 /**
  *  连连支付
- @param param     data:{@"type":支付方式  quick快捷支付/verify认证支付 , 其他参数}
-                  callback:回调方法
- 其他参数：
- 必须参数:sign,sign_type,busi_partner,dt_order,money_order,
-         no_order,name_goods,info_order,id_type,notify_url,
-         oid_partner,card_no,user_id,
-         id_no,acct_name(认证比快捷多这俩参数)
- 可配置参数:[user_info_id_no,user_info_full_name,user_info_mercht_userno,user_info_identify_state,
-          user_info_identify_type,frms_ware_category,user_info_bind_phone,user_info_dt_register]
+ @param param     data:{@"url":统一网关路径}
+ callback:回调方法
  */
 - (void)llPay:(NSDictionary *)param;
+
+/**
+ *  连连签约
+ @param param     data:{data:{@"url":统一网关路径}
+ callback:回调方法
+ */
+- (void)llSign:(NSDictionary *)param;
 
 @end
